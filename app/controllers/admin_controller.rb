8410764ef95365cef583072a254999a0a562fb57
github username <github email address>
2018-09-01 17:04:31 -0400

    Done.

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,17 @@
+class AdminController < ApplicationController
+  def dashboard
+  end
+
+  def stats
+  end
+
+  def financials
+  end
+
+  def settings
+  end
+end
+
+
+# rails g controller admin dashboard stats financials settings --no-test-framework
+## customize the actions if we write all of them after the controller's name

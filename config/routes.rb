bf9947bb5521ad30521405c9427c977eb5081860
github username <ariellenramirez@gmail.com>
2018-09-04 15:46:44 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   resources :accounts
-  get 'admin/dashboard'
-
-  get 'admin/stats'
-
-  get 'admin/financials'
-
-  get 'admin/settings'
-
-  get 'categories/show'
+  get 'accounts/new'
+   get 'accounts/create'
+   get 'accounts/edit'
+   get 'accounts/update'
+   get 'accounts/destroy'
+   get 'accounts/index'
+   get 'accounts/show'
+   get 'admin/dashboard'
+   get 'admin/stats'
+   get 'admin/financials'
+   get 'admin/settings'
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
   resources :categories
